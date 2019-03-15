8ae6fb37a61d0647bed82262acf634eaf6c47bb5
Md. Al-Amin <alamin.fits@gmail.com>
2019-03-15 11:22:52 +0600

    Firebase analytics added.

--- FCMExample/app/src/main/java/com/example/fcmexample/MainActivity.java
+++ FCMExample/app/src/main/java/com/example/fcmexample/MainActivity.java
@@ -3,11 +3,28 @@ package com.example.fcmexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        String id = "201531043015";
+        String name = "Md. Al-Amin";
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
     }
 }
